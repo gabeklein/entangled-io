a564dafc5a72ef015076f1254a49c1c27ad86bbb
Gabe <gabe@termtm.com>
2020-02-10 22:14:23 -0500

    remove dead code

--- lib/webpack/src/index.ts
+++ lib/webpack/src/index.ts
@@ -1,10 +1,7 @@
 import { Compiler } from "webpack";
 import path from "path";
 import fs from "fs";
-import { bundle } from "@entangled/interface"
-// import * as TJS from "typescript-json-schema";
-
-// import parser from "@typescript-eslint/parser";
+// import { bundle } from "@entangled/interface"
 
 const PLUGINID = "RemoteFunctionReplacementPlugin";
 
@@ -26,21 +23,6 @@ function rootDevDependancies(module: string){
     return new Set(deps ? Object.keys(deps) : []);
 }
 
-// function openFile(path: string){
-//     if(!fs.existsSync(path))
-//         throw new Error(`Could not find module's package.json`)
-
-//     if(!fs.lstatSync(path).isFile())
-//         throw new Error(`Requested file ${path} is not a file.`)
-
-//     return new Promise((resolve, reject) => {
-//         fs.readFile(path, { encoding: "utf-8" }, (err, file) => {
-//             if(err) reject(err);
-//             else resolve(file);
-//         })
-//     });
-// }
-
 module.exports = class RemoteFunctionReplacementPlugin {
 
     devDeps?: Set<string>;
@@ -49,73 +31,32 @@ module.exports = class RemoteFunctionReplacementPlugin {
     }
 
     apply(compiler: Compiler) {
-
         compiler.hooks.entryOption.tap(PLUGINID, 
             (context: string, entries: any) => {
                 this.devDeps = rootDevDependancies(context);
             }
         )
 
-        //gain access to module construction
         compiler.hooks.normalModuleFactory.tap(PLUGINID, (compilation: any) => {
-            //when a module is requested, but before webpack looks for it in filesystem
-            // compilation.hooks.beforeResolve.tap(PLUGINID, (result: any) => {
-            //     const target = result.dependencies[0];
-            //     const {
-            //         request
-            //     } = result;
-
-            //     // if(this.devDeps!.has(request))
-            //     //     debugger
-
-            //     // debugger
-
-            //     void request, target
-            // });
-
             compilation.hooks.afterResolve.tapAsync(PLUGINID, (result: any, cb: () => void) => {
+                const { rawRequest, request } = result;
 
-                const maybeTypeFile = result.request.replace(/\.js$/, ".d.ts");
+                if(this.devDeps!.has(rawRequest)){
+                    const {
+                        descriptionFileData: pkg,
+                        descriptionFileRoot: root,
+                        relativePath: main
+                    } = result.resourceResolveData;
 
-                if(this.devDeps!.has(result.rawRequest)){
+                    // const maybeTypeFile = main.replace(/\.js$/, ".d.ts");
+                    // const contents = bundle(root, maybeTypeFile, rawRequest)
 
-                    const contents = bundle(maybeTypeFile, result.rawRequest)
-
-                    void contents;
+                    void main, pkg, root, request;
 
                     debugger
-
-
-                    // const contents = openFile(maybeTypeFile).then(() => {
-                    //     const parser = require("@typescript-eslint/parser");
-                    //     debugger
-                    //     const stuff = parser.parse(contents, {
-                    //         // tsconfigRootDir: result
-                    //         // filePath: maybeTypeFile
-                            
-                    //     })
-                    //     void stuff
-                    //     debugger
-                    //     // debugger
-                    // })
-
-                    // debugger
-
-                    // const parser = require("@typescript-eslint/parser");
-
-                    // const stuff = parser.parse(contents, {
-                    //     // tsconfigRootDir: result
-                    //     // filePath: maybeTypeFile
-                        
-                    // })
-                    // void stuff;
-
-                    // debugger
                 }
 
                 else cb();
-
-                void maybeTypeFile;
             });
         });
     }
