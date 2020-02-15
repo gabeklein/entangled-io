215ba93f14ec29dc1296bf7056e2b0dc23d79ff1
Gabe <gabe@termtm.com>
2020-02-14 21:27:31 -0500

    integrate native webpack resolver

--- lib/webpack/src/index.ts
+++ lib/webpack/src/index.ts
@@ -1,63 +1,66 @@
 import { Compiler } from "webpack";
 import path from "path";
-import fs from "fs";
+import { collateTypes } from "@entangled/interface"
+
+const resolve = require("enhanced-resolve");
 // import { bundle } from "@entangled/interface"
+import { appendToFilesystem } from './stats';
 
 const PLUGINID = "RemoteFunctionReplacementPlugin";
 
 export const toArray = <T> (value: T | Array<T>): Array<T> =>
     value ? Array.isArray(value) ? value : [value] : [];
 
-function rootDevDependancies(module: string){
-    const pkg = path.resolve(module, "package.json")
-
-    if(!fs.existsSync(pkg))
-        throw new Error(`Could not find module's package.json`)
-
-    if(!fs.lstatSync(pkg).isFile())
-        throw new Error(`How is package.json not a file here?`)
-
-    const pkgjson = fs.readFileSync(pkg, "utf-8");
-    const { devDependencies: deps } = JSON.parse(pkgjson);
-    
-    return new Set(deps ? Object.keys(deps) : []);
-}
-
 module.exports = class RemoteFunctionReplacementPlugin {
 
-    devDeps?: Set<string>;
+    devDeps = new Map<string, string>();
 
     constructor(options: any){
+        for(const mod of ["@entangled/service"])
+            this.devDeps.set(mod, "")
     }
 
     apply(compiler: Compiler) {
         compiler.hooks.entryOption.tap(PLUGINID, 
             (context: string, entries: any) => {
-                this.devDeps = rootDevDependancies(context);
+                const typeResolver = resolve.create.sync({
+                    extensions: [".d.ts"],
+                    mainFields: ["types", "main"],
+                    resolveToContext: false,
+                    symlinks: true
+                });
+                
+                for(const request of this.devDeps.keys())
+                    try {
+                        let resolved = typeResolver(context, request)
+                        this.devDeps.set(request, resolved.replace("/lib/index.d.ts", ""));
+                    }
+                    catch(err){
+                        throw new Error("Couldn't find types")
+                    }
             }
         )
 
         compiler.hooks.normalModuleFactory.tap(PLUGINID, (compilation: any) => {
-            compilation.hooks.afterResolve.tapAsync(PLUGINID, (result: any, cb: () => void) => {
-                const { rawRequest, request } = result;
+            compilation.hooks.beforeResolve.tap(PLUGINID, (result: any) => {
+                if(this.devDeps!.has(result.request)){
+                    const context = this.devDeps.get(result.request)!;
+                    const virtual = path.join(context, "entangled-agent.js");
+                    const stuff = collateTypes(context);
 
-                if(this.devDeps!.has(rawRequest)){
-                    const {
-                        descriptionFileData: pkg,
-                        descriptionFileRoot: root,
-                        relativePath: main
-                    } = result.resourceResolveData;
-
-                    // const maybeTypeFile = main.replace(/\.js$/, ".d.ts");
-                    // const contents = bundle(root, maybeTypeFile, rawRequest)
-
-                    void main, pkg, root, request;
-
+                    void stuff;
                     debugger
-                }
+                    const initContent = "module.exports = { foo: \'bar\' }"
 
-                else cb();
-            });
+                    appendToFilesystem(
+                        compiler.inputFileSystem, 
+                        virtual, 
+                        initContent
+                    );
+
+                    result.request = virtual;
+                }
+            })
         });
     }
 }
