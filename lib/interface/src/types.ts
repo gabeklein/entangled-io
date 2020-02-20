2360a93617d064c84b16fc851f1ecfddd49a390c
Gabe <gabe@termtm.com>
2020-02-20 11:51:41 -0500

    simplify types

--- lib/interface/src/types.ts
+++ lib/interface/src/types.ts
@@ -1,39 +1,26 @@
 export namespace Entangled {
-  type Parameter = 
-    | string
-    | number
-    | boolean
-    | Date
+  type HavingDefault = { default: Fn };
 
-  type Data = 
-    | Parameter
-    | { [ key: string ]: Data }
-
-  type GetFunction = (...params: any[]) => any;
-  type PostFunction = (data: Data) => any;
-  
-  type IOF = GetFunction | PostFunction;
-  type HavingDefault = { default: IOF };
+  type Fn = (...args: any[]) => any;
+  type AsyncFn = (...args: any[]) => Promise<any>;
 
   type ArgumentsOf<T> = T extends (... args: infer U ) => infer R ? U: never;
-  type RemoteCallable<F extends IOF> = (...args: ArgumentsOf<F>) => Promise<ReturnType<F>>
+  type RemoteSync<F extends Fn> = (...args: ArgumentsOf<F>) => Promise<ReturnType<F>>
 
   type Item<T> = 
-    T extends Function ? 
-      T extends IOF ? 
-        RemoteCallable<T> :
-        never :
+    T extends AsyncFn ? T :
+    T extends Fn ? RemoteSync<T> :
     T extends {} ? API<T> : 
     never;
 
-  export type Namespace<T extends {}> = { [P in keyof T]: Item<T[P]> };
+  export type Namespace<T extends {}> = { readonly [P in keyof T]: Item<T[P]> };
 
   export type CallableNamespace<T extends HavingDefault> = 
-    RemoteCallable<T["default"]> & 
+    RemoteSync<T["default"]> & 
     Namespace<Omit<T, "default">>
 
   export type DefineRoute = 
-    | IOF 
+    | Fn 
     | { [k: string]: DefineRoute }
 
   /*Entangled.API*/
