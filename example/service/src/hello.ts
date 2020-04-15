5f8d0b605d4abbdd52de4dafd90d336818ff8f95
Gabe <gabe@termtm.com>
2020-04-15 11:48:33 -0400

    add comments to hello module

--- example/service/src/hello.ts
+++ example/service/src/hello.ts
@@ -1,14 +1,24 @@
+/**
+ * A "default" export is mapped to the root of a route!
+ * 
+ * You can call this directly, as `API.greetings()` as opposed to `API.greetings.default()`
+ */
 export default function greetings(){
   return `Greetings lone traveller!`
 }
 
+/**
+ * Callable as `API.greetings.hi()`
+ */
 export function hi(){
   return "Hello World!"
 }
 
+/**
+ * Callable as `API.greetings.hello("Me", new Date("01/01/1970"))`
+ */
 export async function hello(
   name: string,
-  age: number,
   birthday: Date){
 
   return `Hello ${name}!!`
