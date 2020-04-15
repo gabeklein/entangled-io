c4fe51cf1a8d0052a70e3afd2ff2203657e564e4
Gabe <gabe@termtm.com>
2020-04-15 12:33:07 -0400

    enhanse hello examples

--- example/service/src/hello.ts
+++ example/service/src/hello.ts
@@ -15,11 +15,20 @@ export function hi(){
 }
 
 /**
- * Callable as `API.greetings.hello("Me", new Date("01/01/1970"))`
+ * Callable as `API.greetings.hello("Me", new Date("01/01/1970"))`;
+ * 
+ * Working with dates is super easy with entanged. 👀
  */
 export async function hello(
-  name: string,
-  birthday: Date){
+  name: string, birthday: Date){
 
-  return `Hello ${name}!!`
+  const today = new Date();
+  let quote = `Hello ${name}!`
+
+  if(today.getMonth() == birthday.getMonth()
+  && today.getDate() == birthday.getDate()){
+    quote += " Happy Birthday! 🥳🎁"
+  }
+  
+  return quote;
 }
