a7d4525022518d900e687296cc9ca1f3b18a2201
Gabe Klein <gabe@termtm.com>
2022-01-01 20:21:03 -0500

    collapse helper function

--- packages/webpack/src/generate.ts
+++ packages/webpack/src/generate.ts
@@ -1,30 +1,23 @@
 import path from 'path';
 
 import { getSchemaFromSource } from './scanner';
-import { Recursive, ReplacedModule } from './types';
+import { ReplacedModule } from './types';
 
 export function generateServiceAgent(target: ReplacedModule, agent: string){
   const { sourceFile, location, name } = target;
-  const { output, endpoint } = getSchemaFromSource(sourceFile!, name);
+  let { output, endpoint } = getSchemaFromSource(sourceFile!, name);
   
   const filename = path.join(location!, "service_agent.js");
-  const content = generateServiceAgentContent(agent, endpoint, output);
+  const data = JSON.stringify(output);
+
+  if(/^[/A-Z]+$/.test(endpoint))
+    endpoint = `process.env.${endpoint}`;
+
+  const content =
+    `module.exports = require("${agent}")(${data}, ${endpoint})`;
 
   return {
     filename,
     content
   }
-}
-
-function generateServiceAgentContent(
-  agent: string,
-  endpoint: string,
-  schema: Recursive){
-
-  const data = JSON.stringify(schema);
-
-  if(/^[/A-Z]+$/.test(endpoint))
-    endpoint = `process.env.${endpoint}`;
-
-  return `module.exports = require("${agent}")(${data}, ${endpoint})`
 }
