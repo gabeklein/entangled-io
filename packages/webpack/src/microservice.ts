2ccb56596bb9cb59c5838a8144e5afc1e14bad47
Gabe Klein <gabe@termtm.com>
2022-01-08 12:57:11 -0500

    collect excluded modules by child compiler

--- packages/webpack/src/microservice.ts
+++ packages/webpack/src/microservice.ts
@@ -21,28 +21,55 @@ export class RuntimeEntryPlugin {
   }
 }
 
+type PackageJSON = {
+  name: string;
+  version: string;
+}
+
+type Dependancies = {
+  [name: string]: string;
+}
+
 export class ExternalNodeModulesPlugin {
+  constructor(
+    public report?: (deps: Dependancies) => void
+  ){}
+
   apply(compiler: Compiler){
     const NAME = this.constructor.name;
+    let dependencies = {} as { [pkg: string]: string };
 
-    compiler.hooks.compile.tap(NAME, ({ normalModuleFactory }) => {
-      normalModuleFactory.hooks.factorize.tapAsync(NAME, (data, callback) => {
+    compiler.hooks.compile.tap(NAME, (params) => {
+      const factory = params.normalModuleFactory;    
+      dependencies = {};  
+
+      factory.hooks.factorize.tapAsync(NAME, (data, callback) => {
         const module = data.dependencies[0].request;
-        const resolver = normalModuleFactory.getResolver("normal", data.resolveOptions);
+        const resolver = factory.getResolver("normal", data.resolveOptions);
         const resolveContext = {
           fileDependencies: data.fileDependencies,
           missingDependencies: data.missingDependencies,
           contextDependencies: data.contextDependencies
         };
 
-        resolver.resolve({}, data.context, module, resolveContext, (_err, result) => {
-          if(result && /node_modules/.test(result))
-            callback(null,
-              new ExternalModule(module, "commonjs", module)
-            );
+        resolver.resolve({}, data.context, module, resolveContext, (_err, result, info) => {
+          if(result && /node_modules/.test(result)){
+            const { name, version } = info!.descriptionFileData as PackageJSON;
+            const external = new ExternalModule(name, "commonjs", name);
+
+            dependencies[name] = version;
+            callback(null, external);
+          }
           else
             callback();
         });
+      })
+    })
+
+    compiler.hooks.make.tap(NAME, (compilation) => {
+      compilation.hooks.seal.tap(NAME, () => {
+        if(this.report)
+          this.report(dependencies);
       })
     })
   }
