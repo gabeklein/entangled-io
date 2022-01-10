b5ac59ea5bb1351c77dfe8978bbac2c55446722e
Gabe Klein <gabe@termtm.com>
2022-01-10 01:16:48 -0500

    separate module name from namespace

--- packages/webpack/src/EntangledApi.ts
+++ packages/webpack/src/EntangledApi.ts
@@ -9,7 +9,7 @@ import MicroservicePlugin from './Microservice';
 interface ReplacedModule {
   name: string;
   request: string;
-  watchFiles: Set<string>;
+  watch: Set<string>;
   sourceFile: SourceFile;
   location: string;
   filename: string;
@@ -106,6 +106,7 @@ export default class ApiReplacementPlugin {
           return;
         }
 
+        const uid = hash(result.request, 6);
         const namespace = this.shouldInclude({
           type: target.type,
           issuer: result.contextInfo.issuer,
@@ -119,8 +120,8 @@ export default class ApiReplacementPlugin {
             throw new Error(`Tried to import ${relative} (as external) but is not typescript!`);
           }
 
-          const mock = this.loadRemoteModule(resolved, namespace);
-          this.microservicePlugin.include(resolved, namespace);
+          const mock = this.loadRemoteModule(resolved, namespace, uid);
+          this.microservicePlugin.include(resolved, uid);
 
           useInstead(mock);
         }
@@ -141,7 +142,7 @@ export default class ApiReplacementPlugin {
         return;
 
       this.replacedModules.forEach(mod => {
-        mod.watchFiles.forEach(file => {
+        mod.watch.forEach(file => {
           compilation.fileDependencies.add(file)
         })
       })
@@ -161,7 +162,7 @@ export default class ApiReplacementPlugin {
         let updates = 0;
 
         for(const name of filesUpdated)
-          if(mod.watchFiles.has(name))
+          if(mod.watch.has(name))
             updates++;
 
         if(updates)
@@ -202,7 +203,7 @@ export default class ApiReplacementPlugin {
     return null;
   }
 
-  loadRemoteModule(request: string, name = "default"){
+  loadRemoteModule(request: string, name: string, namespace = "default"){
     const tsc = this.tsProject;
     const sourceFile = tsc.addSourceFileAtPath(request);
 
@@ -212,12 +213,12 @@ export default class ApiReplacementPlugin {
     const filename = path.join(location, `${name}.proxy.js`);
 
     const mod: ReplacedModule = {
-      name,
+      name: namespace,
       request,
       location,
       sourceFile,
       filename,
-      watchFiles: new Set()
+      watch: new Set()
     };
 
     this.replacedModules.set(request, mod);
@@ -227,11 +228,11 @@ export default class ApiReplacementPlugin {
   }
   
   writeReplacement(mod: ReplacedModule){
-    const output = createManifest(mod.sourceFile, mod.watchFiles);
+    const output = createManifest(mod.sourceFile, mod.watch);
     const data = JSON.stringify(output);
 
     const {
-      endpoint = "http://localhost:8080",
+      endpoint,
       agent = DEFAULT_AGENT
     } = this.options;
 
@@ -244,4 +245,28 @@ export default class ApiReplacementPlugin {
       `module.exports = require("${agent}")(${data}, ${options})`  
     );
   }
+}
+
+/**
+ * Hashing ("cyrb53") solution from stack-overflow. Avoids the use of `crypto`.
+ * 
+ * https://stackoverflow.com/a/52171480/877165
+ */
+ export function hash(str = "", length: number){
+  const m32 = Math.imul;
+  const x = 0x85ebca6b, y = 0xc2b2ae35;
+  let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
+
+  for(let i = 0, ch; i < str.length; i++){
+    ch = str.charCodeAt(i);
+    h1 = m32(h1 ^ ch, 0x9e3779b1);
+    h2 = m32(h2 ^ ch, 0x5f356495);
+  }
+
+  h1 = m32(h1 ^ (h1>>>16), x) ^ m32(h2 ^ (h2>>>13), y);
+  h2 = m32(h2 ^ (h2>>>16), x) ^ m32(h1 ^ (h1>>>13), y);
+
+  const out = 0x100000000 * (0x1fffff & h2) + (h1>>>0);
+
+  return out.toString(16).substring(0, length).toUpperCase();
 }
