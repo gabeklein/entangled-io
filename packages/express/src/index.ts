a6560aaeaec23b23043c327f84352f09fa14d1ac
Gabe Klein <gabe@termtm.com>
2022-01-11 11:01:03 -0500

    rename start to bootstrap

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,4 +1,4 @@
 export { default as Entangled } from '@entangled/interface';
 export { Service } from './interface';
 export { getContext } from './async_hook';
-export { default } from "./start";
+export { default } from "./bootstrap";
