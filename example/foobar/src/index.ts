5816b9783494da51d20a8bad066d7b6c22f3f6e3
Gabe Klein <gabe@termtm.com>
2022-01-16 23:14:53 -0500

    remove foobar

--- example/foobar/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A "default" export is mapped to the root of a route!
- * 
- * You can call this directly, as `API.greetings()` as opposed to `API.greetings.default()`
- */
- export async function greetings(){
-  return `Greetings lone traveller!`
-}
