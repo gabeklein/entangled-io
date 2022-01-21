7508f1a6a3933f7e0997356dcafa82a341628791
Gabe Klein <gabe@termtm.com>
2022-01-21 11:37:53 -0500

    implement error forwarding and context hook

--- packages/express/src/abstract.ts
+++ packages/express/src/abstract.ts
@@ -1,6 +1,6 @@
-import { RequestHandler, Response } from 'express';
+import { RequestHandler } from 'express';
 
-import { Internal, RestError, BadInput } from './errors';
+import { Internal, BadInput, emitCustomError } from './errors';
 import { format, parse } from './body';
 import { createContext } from './async_hook';
 
@@ -8,57 +8,44 @@ export function abstract(
   handler: Function): RequestHandler {
 
   return async (request, response) => {
-    try {
-      let { body } = request;
-
-      if(!body)
-        body = []
-      else if(Array.isArray(body) ==  false)
-        throw BadInput("POST body must be an array")
-
-      body = parse(body);
-
-      createContext({ req: request, res: response });
-
-      let output = await handler.apply(null, body);
-
-      try { 
-        responder(response, 200, output);
-      }
-      catch(err){
-        throw Internal(
-          "Resource returned data which could not be serialized",
-          "serialize_error"
-        );
-      }
-    }
-    catch(err){
-      if(err instanceof RestError)
-        responder(response, err.statusCode, {
-          error: err.shortCode,
-          message: err.message
-        });
-
-      else {
-        responder(response, 500, err);
-        throw err;
-      }
-    }    
-  }
-}
-
-function responder(
-  response: Response,
-  status: number,
-  content: any){
-
-  if(response.headersSent)
-    return
-
-  response.status(status)
-  
-  if(content == null || typeof content !== "object")
-    content = { response: content };
+    return createContext(
+      { req: request, res: response },
+      async () => {
+        try {
+          let { body } = request;
     
-  response.json(format(content))
+          if(!body)
+            body = []
+          else if(Array.isArray(body) ==  false)
+            throw BadInput("POST body must be an array")
+    
+          body = parse(body);
+    
+          let output = await handler.call(null, body);
+
+          try { 
+            if(response.headersSent)
+              return;
+
+            response.status(200)
+
+            if(output == null || typeof output !== "object")
+              output = { response: output };
+              
+            response.json(format(output))
+          }
+          catch(err){
+            throw Internal(
+              "Resource returned data which could not be serialized",
+              "serialize_error"
+            );
+          }
+        }
+        catch(err){
+          if(err instanceof Error)
+            emitCustomError(err, response);
+        } 
+      }
+    );
+  }
 }
--- packages/express/src/errors.ts
+++ packages/express/src/errors.ts
@@ -1,6 +1,38 @@
+import { Response } from 'express';
+
+const CustomErrors = new Map<typeof Error, string>();
+
+export function setCustomError(
+  Type: typeof Error,
+  path: string){
+
+  CustomErrors.set(Type, path);
+}
+
+export function emitCustomError(
+  error: Error | RestError, response: Response){
+
+  const statusCode =
+    "status" in error ? error.status : 500;
+
+  const info = {} as any;
+
+  for(const key of Object.getOwnPropertyNames(error))
+    info[key] = (error as any)[key];
+
+  const typeIdentifier =
+    CustomErrors.get((error as any).constructor);
+
+  if(typeIdentifier)
+    info.uid = typeIdentifier;
+
+  response.status(statusCode);
+  response.json(info);
+}
+
 export class RestError extends Error {
   constructor(
-    public statusCode: number,
+    public status: number,
     message?: string, 
     public shortCode?: string){
 
