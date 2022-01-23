76f6660db45c824e64a85b1e526209c85abc3640
Gabe Klein <gabe@termtm.com>
2022-01-23 03:14:05 -0500

    add CORS to example

--- example/service/src/index.ts
+++ example/service/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Handler } from "express";
 import { Service } from "@entangled/express";
 import * as Api from "@example/api";
 
@@ -7,11 +7,25 @@ async function run(){
   const entangled = new Service({ Api });
   const app = express();
 
+  app.use(origin());
   app.use(entangled.routes());
 
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
   }) 
+}
+
+function origin(): Handler {
+  return (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+  
+    if(req.method === 'OPTIONS')
+      res.sendStatus(200)
+    else
+      next();
+  }
 }
 
 run();
