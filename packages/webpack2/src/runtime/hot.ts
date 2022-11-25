c42eb87f436bf5a493512402d048d369c2e91de7
Gabe Klein <gabe@termtm.com>
2022-11-25 12:37:16 -0500

    separate require runtime logic

--- packages/webpack2/src/runtime/hot.ts
+++ packages/webpack2/src/runtime/hot.ts
@@ -1,6 +1,5 @@
-import { logApplyResult, logFailedReload } from "./logs";
-
-const FUNCTION_REGISTER = new Map<string, Map<string, Function>>();
+import { logApplyResult, logFailedReload } from './logs';
+import { webpackRequireCallback } from './require';
 
 module.exports = (webpackRequire: any) => {
   webpackRequire.i.push(webpackRequireCallback);
@@ -57,66 +56,6 @@ function addWebpackUpdateListener(webpackRequire: any){
       logFailedReload(err as Error, hot);
     }
   }
-}
-
-function webpackRequireCallback(options: any){
-  // const isEntryFile = options.module.parents.length === 0;
-  const { module, id } = options;
-  const { hot } = module as any;
-
-  if(!hot)
-    return;
-
-  let exports = {};
-  
-  Object.defineProperty(module, "exports", {
-    configurable: true,
-    get: () => {
-      if(Object.keys(exports).length){
-        exports = bootstrap(id, exports);
-        Object.defineProperty(module, "exports", {
-          value: exports
-        })
-      }
-      
-      return exports;
-    }
-  })
-  
-  hot.accept((error: Error, mod: any) => {
-    console.warn(`Loading module '${id}' failed due to error. Refresh module to try again.`);
-    // debugger
-    // hot.decline();
-  });
-}
-
-function bootstrap(id: string, exports: any){
-  let register = FUNCTION_REGISTER.get(id);
-
-  if(!register)
-    FUNCTION_REGISTER.set(id, register = new Map());
-
-  const proxyExports = {};
-
-  for(const name in exports){
-    const value = exports[name];
-
-    if(typeof value == "function"){
-      const proxy = (...args: any[]) => (
-        register!.get(name)!.apply(null, args)
-      );
-
-      register.set(name, value);
-      Object.defineProperty(proxyExports, name, {
-        enumerable: true,
-        get: () => proxy
-      })
-    }
-  }
-
-  console.log(`Loaded module: ${id}`);
-
-  return proxyExports;
 }
 
 function evaluate(code: string){
---
+++ packages/webpack2/src/runtime/require.ts
@@ -0,0 +1,69 @@
+const FUNCTION_REGISTER = new Map<string, Map<string, Function>>();
+
+export function webpackRequireCallback(options: any){
+  const isEntryFile = options.module.parents.length === 0;
+  const { module, id } = options;
+  const { hot } = module as any;
+
+  if(!hot)
+    return;
+
+  let exports = {};
+  
+  Object.defineProperty(module, "exports", {
+    configurable: true,
+    get: () => {
+      if(Object.keys(exports).length){
+        exports = bootstrap(id, exports);
+        Object.defineProperty(module, "exports", {
+          value: exports
+        });
+
+        if(isEntryFile)
+          debugger
+      }
+      
+      return exports;
+    }
+  });
+  
+  hot.accept((error: Error, mod: any) => {
+    console.warn(`Loading module '${id}' failed due to error. Refresh module to try again.`);
+    // debugger
+    // hot.decline();
+  });
+}
+
+function bootstrap(id: string, exports: any){
+  let register = FUNCTION_REGISTER.get(id);
+
+  if(!register)
+    FUNCTION_REGISTER.set(id, register = new Map());
+
+  const proxyExports = {};
+
+  for(const name in exports){
+    const value = exports[name];
+
+    if(typeof value == "function"){
+      const proxy = (...args: any[]) => (
+        register!.get(name)!.apply(null, args)
+      );
+
+      register.set(name, value);
+      Object.defineProperty(proxyExports, name, {
+        enumerable: true,
+        get: () => proxy
+      })
+    }
+    else
+      Object.defineProperty(proxyExports, name, {
+        enumerable: true,
+        get: () => value
+      })
+  }
+
+  console.log(`Loaded module: ${id}`);
+
+  return proxyExports;
+}
