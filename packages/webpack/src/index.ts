2bb9b2a6878b509542bdd9c7dd09735c364a5e6b
Gabe Klein <gabe@termtm.com>
2022-11-27 18:07:18 -0500

    remove service plugins from v1 webpack module

--- packages/webpack/src/index.ts
+++ packages/webpack/src/index.ts
@@ -1,11 +1,7 @@
 import MicroservicePlugin from './MicroservicePlugin';
 import ServiceAgentPlugin from './ServiceAgentPlugin';
-import DevServerPlugin from './servicePlugin/DevServerPlugin';
-import ServicePlugin from './servicePlugin/ServicePlugin';
 
 export {
-  DevServerPlugin,
   MicroservicePlugin,
   ServiceAgentPlugin,
-  ServicePlugin,
 }
