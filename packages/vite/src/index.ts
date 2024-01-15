faa5c71ab6773cb2e1412cdfc3ce4df117ba9e9b
Gabe Klein <gabe@termtm.com>
2024-01-15 07:53:22 -0500

    implement vite plugin

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -1,9 +1,7 @@
-import { Project, SourceFile, ts } from 'ts-morph';
+import { Node, Project, ts } from 'ts-morph';
 import Vite from 'vite';
 
-import { parse } from './manifest';
-
-const DEFAULT_AGENT = '@entangled/fetch'; // Update as per actual default agent path
+const DEFAULT_AGENT = require.resolve("./runtime/fetch");
 
 interface Options {
   include?: RegExp | string;
@@ -13,93 +11,150 @@ interface Options {
   runtimeOptions?: {}
 }
 
-interface ReplacedModule {
-  name?: string;
-  request?: string;
-  watch: Set<string>;
-  sourceFile?: SourceFile;
-  location?: string;
-  filename?: string;
-}
-
 function ServiceAgentPlugin(options: Options = {}): Vite.Plugin {
-    const {
-      agent = DEFAULT_AGENT,
-      endpoint,
-      runtimeOptions
-    } = options;
+  const {
+    agent = DEFAULT_AGENT,
+    endpoint = "/api"
+  } = options;
 
-  const replacedModules = new Map<string, ReplacedModule>();
-  const tsConfigFilePath =
-    ts.findConfigFile(process.cwd(), ts.sys.fileExists);
-  
+  const entangled = new Map<string, {
+    namespace: string;
+    resolved: string;
+    baseUrl: string;
+  }>();
+
   const tsProject = new Project({
     skipAddingFilesFromTsConfig: true,
-    tsConfigFilePath
+    tsConfigFilePath: ts.findConfigFile(
+      process.cwd(), ts.sys.fileExists
+    )
   });
 
-  function agentModule(resolved: string){
-    const watch = new Set<string>([resolved]);
+  function agentModule(id: string){
+    const module = entangled.get(id);
+
+    if(!module)
+      return;
+
+    const { namespace, resolved } = module;
+
+    // const watch = new Set<string>([resolved]);
     const sourceFile = tsProject.addSourceFileAtPath(resolved);
 
-    replacedModules.set(resolved, { watch });
     tsProject.resolveSourceFileDependencies();
 
-    const manifest = parse(sourceFile, watch);
-    const opts: any = { endpoint, ...runtimeOptions };
-    const args: {}[] = [ manifest ];
+    let handle = "";
+    const code = [] as string[];
+    const exports = sourceFile.getExportedDeclarations();
 
-    if(Object.values(opts).some(x => !!x))
-      args.push(opts);
+    function needsEndpoint(){
+      if(!handle){
+        handle = "rpc";
+        code.push(
+          `import endpoint from "${agent}";\n`,
+          `const ${handle} = endpoint("/${namespace}");\n`
+        );
+      }
 
-    return [
-      `import * as agent from "${agent}";\n`,
-      `const options = ${JSON.stringify(opts)};`,
-      `const manifest = ${JSON.stringify(manifest)};\n`,
-      `const stuff = agent.default(manifest, options);`,
-      `export const Greetings = stuff.Greetings;`,
-      `export const Errors = stuff.Errors;`,
-    ].join("\n");
+      return handle;
+    }
+  
+    for(const [ key, [ value ] ] of exports){
+      if(Node.isSourceFile(value)){
+        const name = `${namespace}/${key}`.toLowerCase();
+        const virtual = `virtual:${name}`;
+
+        entangled.set(virtual, {
+          baseUrl: module.baseUrl,
+          resolved: value.getFilePath(),
+          namespace: name,
+        });
+
+        code.push(`export * as ${key} from "${virtual}";`);
+        continue;
+      }
+
+      if(Node.isFunctionDeclaration(value)){
+        needsEndpoint();
+
+        if(!value.getAsyncKeyword()){
+          code.push(`export const ${key} = () => ${handle}("${key}", { async: false });`);
+          continue;
+        }
+
+        code.push(`export const ${key} = ${handle}("${key}");`);
+        continue;
+      }
+
+      if(isErrorType(value)){
+        needsEndpoint();
+        code.push(`export const ${key} = ${handle}.error("${key}");`);
+      }
+    }
+
+    return {
+      code: code.join("\n"),
+      moduleSideEffects: false
+    }
   }
-
-  const entangled = new Map<string, string>();
 
   return {
     name: 'service-agent-plugin',
     enforce: 'pre',
     async resolveId(source, importer){
-      if(source != "@example/api")
-        return;
+      if(source.startsWith("virtual:"))
+        return source;
 
-      const resolved = await
-        this.resolve(source, importer, { skipSelf: true });
+      if(source == "@example/api"){
+        const namespace = "api";
+        const identifier = `virtual:${namespace}`;
+        const resolved = await
+          this.resolve(source, importer, { skipSelf: true });
 
-      const identifier = `virtual:${source}`;
+        if(resolved)
+          entangled.set(identifier, {
+            baseUrl: endpoint,
+            resolved: resolved.id,
+            namespace
+          });
 
-      if(resolved)
-        entangled.set(identifier, resolved.id);
-
-      return identifier;
-    },
-    load(id){
-      if(id.startsWith("virtual:")){
-        const resolved = entangled.get(id);
-
-        if(resolved){
-          return {
-            code: agentModule(resolved),
-            // syntheticNamedExports: true,
-          }
-        }
+        return identifier;
       }
 
-      return;
+      return null;
+    },
+    load(id){
+      if(id.startsWith("virtual:"))
+        return agentModule(id);
+
+      return null;
     },
     handleHotUpdate({ file, server }){
+      debugger;
       // Handle HMR for dependencies of virtual modules
       // Trigger retransform if necessary
     }
   };
+}
+
+export function isErrorType(node: Node){
+  try {
+    while(Node.isClassDeclaration(node)){
+      const exp = node.getExtendsOrThrow().getExpression();
+      
+      if(Node.isIdentifier(exp))
+        node = exp.getSymbolOrThrow().getValueDeclarationOrThrow();
+      else
+        break;
+    }
+
+    if(Node.isVariableDeclaration(node) && 
+      node.getText() == "Error:ErrorConstructor")
+        return true
+  }
+  catch(err){}
+
+  return false;
 }
 
 export default ServiceAgentPlugin;
---
+++ packages/vite/src/runtime/fetch.ts
@@ -0,0 +1,55 @@
+import { stringify, unpack } from '@entangled/interface';
+
+import { newCustomError, notAsyncError, throwRemoteError } from './errors';
+
+type RestArgument = string | boolean | number | Date;
+
+interface FunctionOptions {
+  async?: boolean;
+}
+
+export default function factory(baseUrl: string){
+  baseUrl = baseUrl.replace(/\/$/, "");
+
+  function rpc(name: string, options: FunctionOptions = {}){
+    if(options.async === false)
+      return notAsyncError(name);
+    
+    const url = (baseUrl + "/" + name).toLowerCase();
+    const Module = {
+      [name](...args: RestArgument[]){
+        return postRequest(url, args);
+      }
+    }
+    
+    return Module[name];
+  }
+
+  function error(path: string){
+    return newCustomError(baseUrl + "/" + path);
+  }
+      
+  return Object.assign(rpc, { error });
+}
+
+async function postRequest<B extends {}>(url: string, body: B){
+  const response = await fetch(url, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      ['Content-Type']: 'application/json',
+      ['Accept']: 'application/json'
+    },
+    body: stringify(body)
+  });
+
+  const output = await response.json().then(unpack);
+  const { status } = response;
+
+  if(status >= 300)
+    throw throwRemoteError(output);
+  else if("response" in output)
+    return output.response;
+  else
+    return output;
+}
