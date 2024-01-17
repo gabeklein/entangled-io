ceda785663d10021dd8753fdb2e99a960f99a0d3
Gabe Klein <gabe@termtm.com>
2024-01-17 16:03:57 -0500

    add cors to api example

--- example/api/src/start.ts
+++ example/api/src/start.ts
@@ -1,5 +1,5 @@
 import service from '@entangled/express';
-import express from 'express';
+import express, { Handler } from 'express';
 
 import * as API from '.';
 
@@ -8,11 +8,22 @@ async function run(){
   const entangled = service(API);
   const app = express();
 
-  app.use("/api", entangled);
+  app.use("/api", cors, entangled);
 
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   }) 
+}
+
+const cors: Handler = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+
+  if(req.method === 'OPTIONS')
+    res.sendStatus(200)
+  else
+    next();
 }
 
 run();
