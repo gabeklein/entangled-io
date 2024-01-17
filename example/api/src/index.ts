e590f4081e1bad73cab68ba46d5028e0c7960425
Gabe Klein <gabe@termtm.com>
2024-01-17 13:53:20 -0500

    rename example Error namespace

--- example/api/src/index.ts
+++ example/api/src/index.ts
@@ -1,4 +1,4 @@
 export * as Greetings from "./greetings";
 export * from "./hello";
-export * as Errors from "./errors";
+export * as Error from "./errors";
 // export * as Hooks from "./hooks";
--- example/client/src/index.ts
+++ example/client/src/index.ts
@@ -1,4 +1,4 @@
-import { Greetings, Errors } from "@example/api";
+import { Greetings, Error } from "@example/api";
 
 window.onload = testHello;
 
@@ -12,22 +12,22 @@ export async function testHello(){
 export async function testErrors(){
   try {
     const response =
-      await Errors.willFailWithInfo("Hello Gabe!");
+      await Error.willFailWithInfo("Hello Gabe!");
 
     alert(`Server said: ${response}!`);
   }
   catch(err){
-    if(err instanceof Errors.SpecialError){
+    if(err instanceof Error.SpecialError){
       const { customInfo } = err;
 
       alert(`Request failed with special error.`);
       alert(`Special error has customInfo: ${customInfo}`)
     }
 
-    if(err instanceof Errors.SuperCustomError)
+    if(err instanceof Error.SuperCustomError)
       alert(`Request failed with super custom error!`);
 
-    if(err instanceof Errors.CustomError)
+    if(err instanceof Error.CustomError)
       alert(`Request failed with special error!`);
   }
 }
