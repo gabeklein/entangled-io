a49716a928608aeea89b737b7094790aad10c50f
Gabe Klein <gabe@termtm.com>
2024-10-22 22:51:02 -0400

    consolidate express package

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,2 +1,35 @@
+import Entangled from '@entangled/interface';
+import * as Express from 'express';
+
+import { abstract } from './abstract';
+import { setCustomError } from './errors';
+
+export function service(exports: {}): Express.Router {
+  const router = Express.Router().use(Express.json());
+
+  function register(
+    handle: Entangled.Schema | Function, 
+    prefix = ""){
+
+    if(typeof handle == "function")
+      router.post(prefix, abstract(handle))
+    else if(handle.prototype instanceof Error)
+      setCustomError(handle as any, prefix);
+    else
+      for(const name in handle){
+        let route = prefix;
+
+        if(name !== "default")
+          route += `/${name}`;
+
+        register(handle[name], route);
+      }
+  }
+  
+  register(exports);
+
+  return router;
+}
+
 export { useContext } from './async_hook';
-export { service as default, service } from './service'
+export default service;
