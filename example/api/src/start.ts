e4439177b52daf5f7d1f4da5f3b2752d5edb5cdb
Gabe Klein <gabe@termtm.com>
2025-04-17 13:56:41 -0400

    use src directly in example api

--- example/api/src/start.ts
+++ example/api/src/start.ts
@@ -1,6 +1,11 @@
-import { serve } from '@entangled/express';
+import { serve } from '@entangled/express/src';
 import * as MODULE_EXPORTS from '.';
 
-serve(MODULE_EXPORTS);
+serve(MODULE_EXPORTS, {
+  port: 8080,
+  onReady(port){
+    console.log("Server started on port", port);
+  }
+});
 
 console.log("Server started.");
