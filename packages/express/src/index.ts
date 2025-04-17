59ab9fd5bc029ccc3215b2b5197a5060572baae0
Gabe Klein <gabe@termtm.com>
2025-04-17 13:52:19 -0400

    separate serve function, better config

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,23 +1,13 @@
-import express from 'express';
-import { router } from './router';
-
-function serve(module: {}, baseUrl = "/api", port = 8080){
-  const app = express();
-  const api = router(module);
-
-  app.use(baseUrl, api);
-
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  }) 
-}
-
-export { useContext } from './async_hook';
+export {
+  useContext
+} from './async_hook';
 
 export {
-  router as service,
   router,
-  serve
-};
+  router as service
+} from './router';
 
-export default serve;
+export {
+  serve,
+  serve as default,
+} from './serve';
---
+++ packages/express/src/serve.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+import { Server } from 'http';
+
+import { router } from './router';
+
+interface Config {
+  port?: number;
+  baseUrl?: string;
+  onError?: (this: Server, error: Error) => void;
+  onReady?: (this: Server, port: number) => void;
+}
+
+declare namespace serve {
+  export { Config };
+}
+
+function serve(module: {}, config: Config | number = {}){
+  if (typeof config === "number")
+    config = { port: config };
+  
+  const {
+    port = 8080,
+    baseUrl = "/",
+    onError,
+    onReady
+  } = config;
+
+  const server = express()
+    .use(baseUrl, router(module))
+    .listen(port);
+
+  if(onReady)
+    server.on("listening", onReady.bind(server, port));
+
+  if(onError)
+    server.on("error", onError.bind(server));
+
+  return server;
+}
+
+export { serve };
